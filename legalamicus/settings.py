2a6ac2702b1fb1b88167c4e885f55c797b373653
Pariksha Chaudhary <parikshachaudhary41@gmail.com>
2026-02-21 17:49:05 +0530

    Enable debug to inspect lawyers error

--- legalamicus/settings.py
+++ legalamicus/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#e547+6i+$6d!k4a%@8s-jr#m4*^4=d0#mttc#8n)i*qexg#sl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
